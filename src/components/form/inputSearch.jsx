fac6aec6e5e0e2147744c5466f07febc7dce27b3
dennisbc24 <dennis.bien.vida@gmail.com>
2024-01-03 17:32:42 -0500

    intento

---
+++ src/components/form/inputSearch.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+/* import './index.css' */
+
+export function InputSearch() {
+    return(
+<input type="text" id='inputSearch'/>
+    )  
+}
+
+export function InputSimple({titulo,tipo}) {
+    return(
+        <>
+        <p>{titulo}</p>
+        <input type={tipo}/>
+        </>
+        
+    )  
+}
+
+export function SelectSimple({children,titulo}) {
+    return(
+        <>
+        <h3>{titulo}</h3>
+        <select name="listSelect">
+            
+            {children}
+        </select>
+        </>
+        
+    )  
+}
+
+export function ParrafoInput({titulo}) {
+    return(
+        <>
+        <h3>{titulo}</h3>
+        <p></p>
+        </>
+        
+    )  
+}
---
+++ src/components/form/titleForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+/* import './index.css' */
+
+export function TitleForm({text}) {
+    return(
+<h1>{text}</h1>
+    )  
+}
+
+
+
