40d504cad2b14ae1e1ccca06ba36920875bf407e
dennisbc24 <dennis.bien.vida@gmail.com>
2024-11-12 16:00:02 -0500

    359

--- src/components/salesForm.jsx
+++ src/components/salesForm.jsx
@@ -5,6 +5,7 @@ import "./salesForm.css";
 import { TitleForm } from "./form/titleForm.jsx";
 import { TableGet } from "./table.jsx";
 import { SalesService } from "../services/sales.js";
+import noImagen from "./img/no_imagen.png";
 const saleService = new SalesService()
 
 export const SelesForm = ({urlBase}) => {
@@ -25,6 +26,12 @@ export const SelesForm = ({urlBase}) => {
   const [showSales, SetShowSales] = useState(true)
   const [textButton, SetTextButton] = useState('Guardar')
   const [allProducts, setAllProducts] = useState([]);
+  const [selectColorUser, SetSelectColorUser] = useState('green');
+  const [selectColorBranch, SetSelectColorBranch] = useState('#009688');
+  const [tableColor, setTableColor] = useState('white');
+
+
+
 
       useEffect(() => {
       // Simula la carga de todos los productos al inicio
@@ -71,15 +78,68 @@ export const SelesForm = ({urlBase}) => {
   }, [count, total]);
 
   useEffect(() => {
-    setRevenue(((PUnit - cost) * count).toFixed(2));
+    const value = ((PUnit - cost) * count).toFixed(2)
+    setRevenue(value)
+    if (value < 0) {
+      setTableColor('red')
+    }
+    else{
+      setTableColor('white')
+    }
   }, [PUnit]);
 
-  const handleCount = ({ target: { value } }) => { setCount(parseInt(value))};
+  const handleCount = ({ target: { value } }) => { 
+    const valueCount = parseInt(value)
+    setCount(valueCount)
+    if (valueCount < 0) {
+      
+    }else{
+
+    }
+
+  };
   const handleTotal = ({ target: { value } }) => {setTotal(parseFloat(value))};
   const handleChange = (e) => {setQuery(e.target.value), setShow(false), SetShowSales(false)}
   const handleDate = (e) =>{setDateSell(e.target.value)}
-  const handleIdUser = (e) =>{  setIdUser(parseInt(e.target.value))}
-  const handleIdBranch = (e) =>{  setIdBranch(parseInt(e.target.value))}
+  
+  const handleIdUser = (e) =>{  
+    const value = parseInt(e.target.value)
+    setIdUser(value)
+    if (value === 1) {
+      SetSelectColorUser('green')
+    }
+    if (value === 2) {
+      SetSelectColorUser('orangered')
+    }
+    if (value === 3) {
+      SetSelectColorUser('yellow')
+    }
+  }
+  
+  const handleIdBranch = (e) =>{  
+    const value = parseInt(e.target.value)    
+    setIdBranch(value)
+    if (value === 1) {
+      SetSelectColorBranch('#009688')
+    }
+    if (value === 3) {
+      SetSelectColorBranch('#ff9800')
+    }
+    if (value === 4) {
+      SetSelectColorBranch('#795548')
+    }
+    if (value === 7) {
+      SetSelectColorBranch('#673ab7')
+    }
+    if (value === 5) {
+      SetSelectColorBranch('#4d589a')
+    }
+    if (value === 6) {
+      SetSelectColorBranch('#c061d1')
+    }
+  }
+    
+  
   const changeCostumer = (e) => {setDataCustomer(e.target.value)}
   const handleClick = (event) => { 
     const textoLi = event.target.textContent
@@ -101,11 +161,11 @@ export const SelesForm = ({urlBase}) => {
       const upload = saleService.register(urlBase, body)
       SetShowSales(true)
     };
-      
+
   return (
     <>
     <TitleForm text='Registrar Venta'></TitleForm>
-      <input type="text" className="only_input"  value={query} onChange={handleChange} placeholder="Buscar..." />
+      <input type="text" className="only_input"  value={query} onChange={handleChange} placeholder="Buscar..."/>
       <ul className="suggestions_lu">   {suggestions.map((suggestion, index) => (
           <li key={index} onClick={handleClick}>
             {suggestion}
@@ -115,61 +175,93 @@ export const SelesForm = ({urlBase}) => {
 
       <div className="divForm">
         <div className="inputs_form">
-        <InputSimple titulo="Fecha" tipo="date" func={handleDate}></InputSimple>
+          <div>
+            <h3>Fecha</h3>
+            <input type="date" onChange={handleDate} className="registrationDate" placeholder="Fecha" required/>
+          </div>
+          
+        
         </div>
         <div className="inputs_form">
         <InputSimple titulo="Sr(a)" tipo="text" func={changeCostumer} callToAction="Cliente"></InputSimple>
-        <SelectSimple titulo="Sucursal"func={handleIdBranch}>
-          <option value="1">B17</option>
-          <option value="3">Departamento</option>
-          <option value="7">Tambopata</option>
-          <option value="4">Deposito</option>
-          <option value="5">Los Nogales</option>
-          <option value="6">Los Incas</option>
-        </SelectSimple>
-        <SelectSimple titulo="Usuario" func={handleIdUser}>
-          <option value="1">Dennis</option>
-          <option value="2">Luz</option>
-          <option value="3">Miguel</option>
-        </SelectSimple>
+        
+        <div className="selectSimple">
+            <h3>Sucursal</h3>
+            <select style={{backgroundColor: selectColorBranch}} name="listSelect" onChange={handleIdBranch} className='only_select'>
+            <option value="1">B17</option>
+            <option value="3">Departamento</option>
+            <option value="4">Deposito</option>
+            <option value="7">Tambopata</option>
+            <option value="5">Los Nogales</option>
+            <option value="6">Los Incas</option>
+            </select>
+          </div>
+          <div className="selectSimple">
+            <h3>Usuario</h3>
+            <select style={{backgroundColor: selectColorUser}} name="listSelect" onChange={handleIdUser} className='only_select'>
+              <option value="1">Dennis</option>
+              <option value="2">Luz</option>
+              <option value="3">Miguel</option>
+            </select>
+          </div>
         </div>
          
       </div>
       <div className="result">
-          <table className="infoTable">
+          <table className="infoTable" >
             <thead>
               <tr>
                 <th>Foto</th>
-                
-                <th>Cant</th>
-                
-                <th>P.U.</th>
-                <th>P.T.</th>
+                <th>Datos</th>
                 <th>Articulo</th>
-                <th>Costo</th>
-                <th>Ganancia</th>
-                <th>Actualizado</th>
-                <th>Creado</th>
-
+                <th>Resultado</th>
+                <th>F. Modif</th>
+                
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td style={{'width':'105px'}}><img className="product_image" src={product.url_image} ></img></td>
-                <td ><input type="number" onChange={handleCount} style={{'width':'30px'}} /></td>
-                <td><p>{`S/.${(PUnit).toFixed(2)}`}</p></td>
-                <td ><input type="number" onChange={handleTotal} style={{'width':'30px'}}  /></td>
-                <td><p style={{'width':'35px'}}>{product.name} </p></td>
-                <td><p>{cost}</p></td>
-                <td><p>{revenue}</p></td>
-                <td><p>{(product.updated)}</p></td>
-                <td><p>{(product.created)}</p></td>
+            <tbody >
+              <tr style={{backgroundColor:tableColor}}>
+                <td style={{'width':'105px'}}><img className="product_image" src={product.url_image ? product.url_image : noImagen} ></img></td>
+                <td style={{'width':'110px'}}>
+                  <div className="dataSell">
+                    <h4>Cant</h4>
+                    <input type="number" onChange={handleCount} style={{'width':'45px'}} />
+                  </div>
+                  <div className="dataSell">
+                    <h4>P.U.</h4>
+                    <p>{`S/.${(PUnit).toFixed(2)}`}</p>
+                    </div>
+                  <div className="dataSell">
+                    <h4>P.T.</h4>
+                    <input type="number" onChange={handleTotal} style={{'width':'45px'}}  />
+                  </div>
+
+                </td>
+ 
+                <td className="td_description" style={{'width':'100px'}} >
+                  <p style={{'width':'35px'}}>{product.name} </p>
+                </td>
+                <td className="td_result" style={{'width':'150px'}} >
+                  
+                    <h4>Costo</h4>
+                    <p>{`S/.${cost}`}</p>
+                    <h4>Ganancia</h4>
+                    <p>{`S/.${revenue}`}</p>
+                </td>
+                
+                <td className="td_modif" style={{'width':'150px'}} >
+                  <h4>Actualizado:</h4>
+                  <p>{(product.updated ? (product.updated.slice(0,10)) : product.updated )}</p>
+                  <h4>Creado:</h4>
+                  <p>{(product.created ? product.created.slice(0,10) : product.created) }</p></td>
+                
               </tr>
               
             </tbody>
           </table>
         </div>
-      <ButtonSave titulo={textButton} func={handleButton}/>
+        <button onClick={handleButton} className="saveSell">Guardar</button>
+      
       <h3>Stock</h3>
       {<>{ show ? <TableGet url={`${urlBase}/api/v1/existence?product=${product.id_product}`}/> : <></>
     }</>}
